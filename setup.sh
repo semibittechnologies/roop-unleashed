9802addb5a9942319b10bdf498a5b4088c79895e
Shivesh Navin <shiveshnavin@gmail.com>
2024-06-30 15:30:29 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+add-apt-repository ppa:deadsnakes/ppa && \
+    apt-get update
+
+
+apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx
+ln -sf /usr/bin/python3.11 /usr/bin/python && \
+    ln -sf /usr/bin/python3.11 /usr/bin/python3
+    
+python --version
+git clone https://github.com/C0untFloyd/roop-unleashed
+cd /roop-unleashed
+pip3 install -r requirements.txt

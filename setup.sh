34e673cf74f038df2fdbe2b6b66c8f99e6c437a3
Shivesh Navin <shiveshnavin@gmail.com>
2024-06-30 21:31:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 # echo pytorch/pytorch:2.1.2-cuda11.8-cudnn8-devel
 # echo https://cloud.vast.ai/?ref_id=143095&template_id=f3db7d14eea13e128908b36395688679
-add-apt-repository ppa:deadsnakes/ppa -y && \
-    apt-get update
+
+# add-apt-repository ppa:deadsnakes/ppa -y && \
+#     apt-get update
 
 
-apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx screen
-ln -sf /usr/bin/python3.11 /usr/bin/python && \
-    ln -sf /usr/bin/python3.11 /usr/bin/python3
+# apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx screen
+# ln -sf /usr/bin/python3.11 /usr/bin/python && \
+#     ln -sf /usr/bin/python3.11 /usr/bin/python3
 
-ln -sf /usr/bin/python3.11 /opt/conda/bin/python && \
-    ln -sf /usr/bin/python3.11 /opt/conda/bin/python3
+# ln -sf /usr/bin/python3.11 /opt/conda/bin/python && \
+#     ln -sf /usr/bin/python3.11 /opt/conda/bin/python3
     
-wget https://bootstrap.pypa.io/get-pip.py && \
-    python3.11 get-pip.py && \
-    rm get-pip.py
+# wget https://bootstrap.pypa.io/get-pip.py && \
+#     python3.11 get-pip.py && \
+#     rm get-pip.py
+
+    
 mkdir /ffmpeg
 cd /ffmpeg && wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz && unxz ffmpeg-release-amd64-static.tar.xz && tar -xvf ffmpeg-release-amd64-static.tar 
 export PATH=/ffmpeg/ffmpeg-7.0.1-amd64-static:$PATH

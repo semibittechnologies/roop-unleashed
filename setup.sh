86037721dd53bdd42fa065985ce432c64c88b3bf
Shivesh Navin <shiveshnavin@gmail.com>
2024-06-30 15:50:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ add-apt-repository ppa:deadsnakes/ppa && \
 apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx ffmpeg screen
 ln -sf /usr/bin/python3.11 /usr/bin/python && \
     ln -sf /usr/bin/python3.11 /usr/bin/python3
-    
+wget https://bootstrap.pypa.io/get-pip.py && \
+    python3.11 get-pip.py && \
+    rm get-pip.py
+
 python --version
-git clone https://github.com/C0untFloyd/roop-unleashed
-cd /roop-unleashed
 pip3 install -r requirements.txt

d67fed5ec9c9cc574a0409738c4d283b80192cdd
Shivesh Navin <shiveshnavin@gmail.com>
2024-06-30 15:32:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ add-apt-repository ppa:deadsnakes/ppa && \
     apt-get update
 
 
-apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx
+apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx ffmpeg
 ln -sf /usr/bin/python3.11 /usr/bin/python && \
     ln -sf /usr/bin/python3.11 /usr/bin/python3
     

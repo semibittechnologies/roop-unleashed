103d43ab88c437250a689e0312660106af3be69b
Shivesh Navin <shiveshnavin@gmail.com>
2024-06-30 19:32:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ add-apt-repository ppa:deadsnakes/ppa && \
     apt-get update
 
 
-apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx ffmpeg screen
+apt-get install -y python3.11 python3.11-venv python3.11-dev libgl1-mesa-glx screen
 ln -sf /usr/bin/python3.11 /usr/bin/python && \
     ln -sf /usr/bin/python3.11 /usr/bin/python3
 wget https://bootstrap.pypa.io/get-pip.py && \

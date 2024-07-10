7295df2250e696d86ddf6e46db1799336e6d38af
semibittechnologies <117454465+semibittechnologies@users.noreply.github.com>
2024-07-10 14:23:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,23 +2,29 @@
 # echo https://cloud.vast.ai/?ref_id=143095&template_id=f3db7d14eea13e128908b36395688679
 
 # Latest: https://cloud.vast.ai/templates/edit?templateHashId=59c8b73967e217815f5b99ea5d80d2a5
-# 11329925
-# Poland, PL
-# 2x RTX A400041.3  TFLOPS
-# m:18551
-# host:78246
+
+# 11454806
+# Age:
+# 16 min.
+# Remaining:
+# 21 days
+# Sweden, SE
+# 1x RTX A600036.1  TFLOPS
+# m:21115
+# host:102965
 # verified
-# 1.6/32.0 GB
-# 364.1 GB/s
-# PRIME B365
-# PCIE 3.0, 16x
-# 12.4 GB/s
-# Pentium® Gold G5420
-# 4.0/4 cpu
-# 2/32 GB
-# Lexar SSD NM620 1TB
-# 553 MB/s6.0/30.3 GB
-# 594.9 Mbps350.4 MbpsMax CUDA: 12.2
+# 0.5/48.0 GB
+# 622.0 GB/s
+# MZ52
+# PCIE 4.0, 8x
+# 12.3 GB/s
+# AMD EPYC 7532 32-Core Processor
+# 8.0/64 cpu
+# 3/64 GB
+# INTEL SSDPE2KX080T8O
+# 5671 MB/s7.0/30.3 GB
+# 52.0 Mbps456.4 MbpsMax CUDA: 12.2
+# GPU: 0% 30C , CPU: 33% Status: success, running pytorch/pytorch_2.1.2-cuda11.8-cudnn8-devel/ssh
 
 
 
